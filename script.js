84b82ab26367bd3be209200c01c96c3ff2c80612
sunwong Kim <face712@naver.com>
2021-08-15 23:58:45 +0900

    8.15 finish

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#loginInput");
 const loginButton = document.querySelector("#loginButton");
-const h1 = document.querySelector("#greeting");
+const greeting = document.querySelector("#greeting");
+const button = document.querySelector("button");
 
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
 
-function loginSubmit(event) {
-  event.preventDefault();
-  const InputValue = loginInput.value;
-
-  console.log(InputValue);
-  loginInput.value = "";
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  h1.innerHTML = "hello " + InputValue;
-  h1.classList.remove(HIDDEN_CLASSNAME);
+function showGreeting() {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = "hello " + referUserName;
 }
 
-loginForm.addEventListener("submit", loginSubmit);
+function inputUserName(event) {
+  event.preventDefault();
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  showGreeting();
+}
+
+const referUserName = localStorage.getItem(USERNAME_KEY);
+if (referUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", inputUserName);
+} else {
+  showGreeting();
+}
+
+// ----------
+function deleteData() {
+  localStorage.removeItem(USERNAME_KEY);
+}
+button.addEventListener("click", deleteData);
