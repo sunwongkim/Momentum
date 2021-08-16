bddde95bab4b3f75c911ce528ef27c1709f5545f
sunwong Kim <face712@naver.com>
2021-08-16 23:57:02 +0900

    add file

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-const images = ["image1.png", "image2.jpg", "image3.jepg"];
+const images = ["image1.png", "image2.jpg", "image3.jpeg"];
+const backgroundImage = document.createElement("img");
+const body = document.querySelector("body");
+body.appendChild(backgroundImage);
+
+const randomNumberImages = Math.floor(String(Math.random() * images.length));
+backgroundImage.src = "img/" + images[randomNumberImages];
+
+console.log(backgroundImage);
