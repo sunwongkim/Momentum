181ff6beaa0b98b21404d51c643472adaf9eb57a
sunwong Kim <face712@naver.com>
2021-08-30 23:22:36 +0900

    새로고침 시 리스트 유지 가능

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const backgroundImage = document.createElement("img");
 const body = document.querySelector("body");
 body.appendChild(backgroundImage);
 
-const randomNumberImages = Math.floor(String(Math.random() * images.length));
+const randomNumberImages = Math.floor(Math.random() * images.length);
 backgroundImage.src = "img/" + images[randomNumberImages];
 
 console.log(backgroundImage);
--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
 // todo .3rd / localStorage-todo 제거
 function deleteToDo(event) {
   console.log(event.target.parentElement);
@@ -22,22 +23,29 @@ function printToDo(toDoInputValue) {
   button.addEventListener("click", deleteToDo);
 }
 
-const toDos = [];
+let toDos = [];
 
 // todo-list .1st / localStorage-todo 저장
 function saveToDo(event) {
   event.preventDefault();
   const toDoInputValue = toDoInput.value;
   toDos.push(toDoInputValue);
-  localStorage.setItem("todos", JSON.stringify(toDos));
-  console.log(toDos);
-  console.log(localStorage.getItem("todos"));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
   toDoInput.value = "";
   printToDo(toDoInputValue);
+  // console.log(toDos);
+  // console.log(localStorage.getItem(TODOS_KEY));
 }
 
 // todo-list 이벤트 시작
 toDoForm.addEventListener("submit", saveToDo);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(printToDo);
+}
 
 // ----------
 // const deleteButton2 = document.querySelector("#DeleteToDo");
