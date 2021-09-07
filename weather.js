c7d2b383f623660621edcf28acd0bd100d326466
sunwong Kim <face712@naver.com>
2021-09-07 21:38:36 +0900

    날씨 추가

--- weather.js
+++ weather.js
@@ -1,15 +1,26 @@
+// 날씨 조회
+// https://openweathermap.org/
 const API_KEY = "9a1734badca6cd605f1a98268cf67962";
 
+// 지리 좌표를 이용한 현재 날씨 조회
 function success(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
-  console.log(position);
-  console.log("you live in", lat, ",", lon);
+  const lat = position.coords.latitude; //위도
+  const lon = position.coords.longitude; //경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; //&units=metric은 화씨=>섭씨 표기
   console.log(url);
-}
-function error() {
-  console.log("bad");
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerHTML = data.name;
+      weather.innerHTML = data.weather[0].main + "  " + data.main.temp;
+    });
 }
 
+function error() {
+  console.log("error");
+}
+
+// 지리 좌표 불러오기
 navigator.geolocation.getCurrentPosition(success, error);
